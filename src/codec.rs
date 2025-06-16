eafc689003d692a638dc7b42b038471d9b3ed061
XTZ206 <ywx2006@outlook.com>
2025-06-17 02:47:57 +0800

    feat(codec): basic keyword codec

---
+++ src/codec.rs
@@ -0,0 +1,27 @@
+fn encode_keyword(databytes: &[u8], keybytes: &[u8]) -> Vec<u8> {
+    let mut buffer: Vec<u8> = Vec::new();
+    for (idx, &databyte) in databytes.iter().enumerate() {
+        let keybyte = keybytes[idx as usize % keybytes.len()];
+        let encoded = databyte.wrapping_add(keybyte);
+        buffer.push(encoded);
+    }
+    return buffer;
+}
+
+fn decode_keyword(databytes: &[u8], keybytes: &[u8]) -> Vec<u8> {
+    let mut buffer: Vec<u8> = Vec::new();
+    for (idx, &databyte) in databytes.iter().enumerate() {
+        let keybyte = keybytes[idx as usize % keybytes.len()];
+        let decoded = databyte.wrapping_sub(keybyte);
+        buffer.push(decoded);
+    }
+    return buffer;
+}
+
+pub fn encode(bytes: &[u8]) -> Vec<u8> {
+    return encode_keyword(bytes, b"VINEGAR");
+}
+
+pub fn decode(bytes: &[u8]) -> Vec<u8> {
+    return decode_keyword(bytes, b"VINEGAR");
+}
